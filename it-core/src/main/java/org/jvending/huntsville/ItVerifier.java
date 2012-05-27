1a9d8003969f7ae7efad20762578443af5180cfc
Shane Isbell <shaneisbell@Shane-Isbells-MacBook-Pro.local>
2012-05-27 16:10:45 -0700

    Methods for clearing and pulling log files.

---
+++ it-core/src/main/java/org/jvending/huntsville/ItVerifier.java
@@ -0,0 +1,63 @@
+package org.jvending.huntsville;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jvending.huntsville.commands.CommandExecutor;
+
+public class ItVerifier
+{
+
+    private static final String SDK = "/Users/shaneisbell/android-sdks/platform-tools/adb";
+    //TODO - make this dynamic (toolchains?)
+
+    public void saveLog( File outputFile )
+        throws Exception
+    {
+        CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
+        String logFile = "/sdcard/it-" + Math.random() + ".txt";
+        List<String> commands = new ArrayList<String>();
+        commands.add( "shell" );
+
+        commands.add( "logcat" );
+
+        commands.add( "-d" );
+
+        commands.add( "-f" );
+        commands.add( logFile );
+
+        executor.executeCommand( SDK, commands, new File( "." ), false );
+
+        pullFile( logFile );
+
+    }
+
+    public void pullFile( String fileName )
+        throws Exception
+    {
+        CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
+        List<String> commands = new ArrayList<String>();
+        commands.add( "pull" );
+
+        commands.add( fileName );
+
+        System.out.println( fileName );
+
+        executor.executeCommand( SDK, commands, new File( "." ), false );
+    }
+
+    public void clearLog()
+        throws Exception
+    {
+        CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
+
+        List<String> commands = new ArrayList<String>();
+        commands.add( "shell" );
+
+        commands.add( "logcat" );
+
+        commands.add( "-c" );
+        executor.executeCommand( SDK, commands, new File( "." ), false );
+    }
+}
---
+++ it-core/src/test/java/org/jvending/huntsville/ItVerifierTest.java
@@ -0,0 +1,49 @@
+package org.jvending.huntsville;
+
+import java.io.File;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ItVerifierTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     * 
+     * @param testName
+     *            name of the test case
+     */
+    public ItVerifierTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ItVerifierTest.class );
+    }
+
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        ItVerifier it = new ItVerifier();
+        it.clearLog();
+    }
+
+    public void testLog()
+        throws Exception
+    {
+        ItVerifier it = new ItVerifier();
+        it.saveLog( new File( "it.log" ) );
+    }
+}
