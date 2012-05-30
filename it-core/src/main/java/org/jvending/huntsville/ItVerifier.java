281f1fc001ec9bf4a288e7882202f2255ddd199e
Shane Isbell <shaneisbell@Shane-Isbells-MacBook-Pro.local>
2012-05-30 11:40:26 -0700

    Add support for tag filters. Additional exception checks.

--- it-core/src/main/java/org/jvending/huntsville/ItVerifier.java
+++ it-core/src/main/java/org/jvending/huntsville/ItVerifier.java
@@ -1,22 +1,54 @@
 package org.jvending.huntsville;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jvending.huntsville.commands.CommandExecutor;
+import org.jvending.huntsville.commands.ExecutionException;
 
 public class ItVerifier
 {
 
     private static final String SDK = System.getProperty( "ANDROID_SDK" ) + "/platform-tools/adb";
 
-    public File saveLog( File tempFileDir, File targetDirectory, LogPriority priority )
-        throws Exception
+    public File saveLog( File tempFileDir, File targetDirectory )
+        throws IOException, ExecutionException
     {
+        return saveLog( tempFileDir, targetDirectory, null, null );
+    }
+
+    public File saveLog( File tempFileDir, File targetDirectory, LogPriority priority )
+        throws IOException, ExecutionException
+    {
+        return saveLog( tempFileDir, targetDirectory, priority, null );
+    }
+
+    public File saveLog( File tempFileDir, File targetDirectory, LogPriority priority, String tag )
+        throws IOException, ExecutionException
+    {
+        if ( tempFileDir == null || targetDirectory == null )
+        {
+            throw new IllegalArgumentException( "Required parameter is null" );
+        }
+
         if ( !tempFileDir.exists() )
         {
-            tempFileDir.mkdirs();
+            if ( !tempFileDir.mkdirs() )
+            {
+                throw new IOException( "Unable to create temp file directory" );
+            }
+        }
+
+        if ( priority == null )
+        {
+            priority = LogPriority.DEBUG;
+        }
+
+        if ( tag == null || tag.isEmpty() )
+        {
+            tag = "*";
         }
 
         File tempFileOnDevice = new File( tempFileDir, "it-" + System.currentTimeMillis() + ".txt" );
@@ -33,23 +65,28 @@ public class ItVerifier
         commands.add( "-f" );
         commands.add( tempFileOnDevice.getAbsolutePath() );
 
-        if ( priority != null )
-        {
-            commands.add( "-s" );
-            commands.add( "*:" + priority.mLevel );
-        }
+        commands.add( "-s" );
+        commands.add( tag + ":" + priority.mLevel );
+
         executor.executeCommand( SDK, commands, new File( "." ), false );
 
-        return pullFile( tempFileOnDevice, targetDirectory );
-
+        return pullFileFromDevice( tempFileOnDevice, targetDirectory );
     }
 
-    public File pullFile( File sourceFileName, File targetDirectory )
-        throws Exception
+    public File pullFileFromDevice( File sourceFileName, File targetDirectory )
+        throws IOException, ExecutionException
     {
+        if ( sourceFileName == null || targetDirectory == null )
+        {
+            throw new IllegalArgumentException( "Required parameter is null" );
+        }
+
         if ( !targetDirectory.exists() )
         {
-            targetDirectory.mkdirs();
+            if ( !targetDirectory.mkdirs() )
+            {
+                throw new IOException( "Unable to create target directory" );
+            }
         }
 
         CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
@@ -65,7 +102,7 @@ public class ItVerifier
     }
 
     public void clearLog()
-        throws Exception
+        throws ExecutionException
     {
         CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
 
@@ -79,7 +116,7 @@ public class ItVerifier
     }
 
     private void deleteLog( File log )
-        throws Exception
+        throws ExecutionException
     {
         CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
 
